82b922cdd70080732d0da6c79ae28154fc1812b4
Rodrigo Moya <rodrigo@gnome-db.org>
2013-12-17 12:12:34 +0100

    Add main library header file

---
+++ gpattern/gpattern.h
@@ -0,0 +1,35 @@
+/* GPattern - GLib software patterns implementation library
+ *
+ * Copyright (C) 2013 Rodrigo Moya
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * Author: Rodrigo Moya <rodrigo@gnome.org>
+ */
+
+#ifndef __G_PATTERN_H__
+#define __G_PATTERN_H__
+
+#define _GPATTERN_GPATTERN_H_INSIDE__
+
+#include <gpattern/gpatterntypes.h>
+
+#include <gpattern/gobservable.h>
+#include <gpattern/gobserver.h>
+
+#undef __GPATTERN_GPATTERN_H_INSIDE__
+
+#endif /* __G_PATTERN_H__ */
